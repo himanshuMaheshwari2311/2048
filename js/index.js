acff5d209440a78b3a7095d0f06a3d36ec2ff24e
himanshuMaheshwari2311 <m.himanshu2311@gmail.com>
2020-05-14 00:47:32 +0530

    Fixing structure and randomizer

--- js/index.js
+++ js/index.js
@@ -1,45 +1,71 @@
 let boardSize = 4;
 
 let gameState = [
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
 ];
+
+const UP = 'UP';
+const DOWN = 'DOWN';
+const LEFT = 'LEFT';
+const RIGHT = 'RIGHT';
+
+let keyPressMap = {
+    37: LEFT,
+    38: UP,
+    39: RIGHT,
+    40: DOWN
+};
 
 $(document).ready(function() {
     renderBoard();
 });
 
+function getTileId(x, y) {
+    return x * boardSize + y;
+}
+
 function renderBoard() {
 
     var board = document.getElementById('board');
-    console.log(board);
 
     for (var i = 0; i < boardSize; i++) {
         var row = $('<tr></tr>').appendTo(board);
         for (var j = 0; j < boardSize; j++) {
-            let text = 4 * i + j;
-            row.append('<td class=\'game-tile\' id=\'' + text + '\'></td>');
+            let id = 4 * i + j;
+            row.append('<td class=\'game-tile\' id=\'' + id + '\'></td>');
         }
     }
 
     // init game state 
-    let startPos1 = Math.floor(Math.random() * 16);
-    let startPos2 = Math.floor(Math.random() * 16);
-    console.log(startPos1);
-    console.log(startPos2);
-    for (; startPos1 == startPos2;) {
-        startPos2 = Math.floor(Math.random() * 16);
+    let x1 = Math.floor(Math.random() * 4);
+    let x2 = Math.floor(Math.random() * 4);
+    let y1 = Math.floor(Math.random() * 4);
+    let y2 = Math.floor(Math.random() * 4);
+    console.log(x1, y1, x2, y2);
+    if (x1 == x2) {
+        x1 == 0 ? x1++ : x1--;
     }
-    gameState[startPos1] = gameState[startPos2] = 1;
 
+    gameState[x1][y1] = gameState[x2][y2] = 2;
+
+    var tile = document.getElementById(getTileId(x1, y1));
+    tile.classList.add('cell-2');
+    tile.textContent = gameState[x1][y1];
+    var tile = document.getElementById(getTileId(x2, y2));
+    tile.classList.add('cell-2');
+    tile.textContent = 2;
     console.log(gameState);
+    playGame();
+}
 
-    var tile = document.getElementById(startPos1);
-    tile.classList.add('cell-2');
-    tile.textContent = 2;
-    var tile = document.getElementById(startPos2);
-    tile.classList.add('cell-2');
-    tile.textContent = 2;
+function playGame() {
+    $(window).keydown(function(event) {
+        if (event.keyCode in keyPressMap) {
+            console.log(keyPressMap[event.keyCode]);
+        }
+
+    })
 }
